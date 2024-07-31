de08cb9e56f9ae98b1fb35b336d4068e216c8c6a
Nikhithakondreddygari <nikhithakondreddygari135@gmail.com>
2024-07-31 10:22:16 +0530

    changes made in user model

--- models/usermodel.js
+++ models/usermodel.js
@@ -1,45 +1,3 @@
-// const mongoose = require('mongoose');
-// const validator = require('validator');
-// const bcryptjs = require('bcryptjs');
-
-// const userSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: [true, 'Please Enter Your Name'],
-//         lowercase: true,
-//         trim: true,
-//         index: true,
-//     },
-//     email: {
-//         type: String,
-//         required: [true, 'Please Enter Your Email'],
-//         unique: true,
-//         lowercase: true,
-//         validate: [validator.isEmail, 'Please enter a valid email']
-//     },
-//     password: {
-//         type: String,
-//         required: [true, 'Please enter a password'],
-//         minlength: 8,
-//         select: false
-//     },
-//     confirmPassword: {
-//         type: String,
-//         required: [true, 'Please confirm your password'],
-//         validate: {
-//             validator: function(value) {
-//                 return value === this.password;
-//             },
-//             message: 'Password and confirm password do not match'
-//         }
-//     },
-//     twoFactorSecret: {
-//         type: String
-//     }
-// }, { timestamps: true });
-
-// module.exports = mongoose.model('User', userSchema);
-
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -63,7 +21,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please enter a password'],
         minlength: 8,
-        select: false // Passwords should not be returned in queries by default
+        select: false 
     },
     twoFactorSecret: {
         type: String
